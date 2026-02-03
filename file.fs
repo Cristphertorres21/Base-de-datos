29406ad255a98980d196fadcb527702420e6d746
Cristophertorres21 <cristophertorresdza@gmail.com>
2026-02-03 17:23:56 -0600

    Actualizacion

--- file.fs
+++ file.fs
@@ -1,24 +1,5 @@
-// 1. Definimos los tipos de datos (Modelado de dominio)
-type Producto = {
-    Nombre: string
-    Precio: deal
-}
+let rec length = function
+    | []      -> 0        
+    | _ :: xs -> 1 + length xs  
 
-
-// 2. Función para calcular el precio final usando Pattern Matching
-let calcularPrecioFinal producto descuento =
-    match descuento with
-    | Porcentaje p -> producto.Precio * (1.0m - p)
-    | ValorFijo v -> producto.Precio - v
-    | SinDescuento -> producto.Precio
-
-// 3. Creamos algunos datos
-let miLaptop = { Nombre = "Laptop Pro"; Precio = 1000.0m }
-let ofertaBlackFriday = Porcentaje 0.20m // 20% de descuento
-
-// 4. Ejecución y salida
-let precioFinal = calcularPrecioFinal miLaptop ofertaBlackFriday
-
-printfn "Producto: %s" miLaptop.Nombre
-printfn "Precio Original: $%M" miLaptop.Precio
-printfn "Precio con Descuento: $%M" precioFinal
+printfn "Longitud de [1; 2; 3; 4]: %d" (length [1; 2; 3; 4])
